e12fbfc59f309bc5f0effa7e90ef4b269044deae
cassianocarli <138841588+cassianocarli@users.noreply.github.com>
2024-06-17 16:47:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// angular.module('blog', [])
-// .controller('Rest', function($scope, $http) {
-//     $http.get('https://api-fake-blog.onrender.com/postagens')
-//     .then(function(response) {
-//         $scope.publicacoes = response.data;
-//     }, function(error) {
-//         console.error('Erro ao buscar as postagens:', error);
-//     });
-// })
-// .controller('NoticiaDetalhesCtrl', function($scope, $http, $location) {
-//     var params = new URLSearchParams($location.absUrl().split('?')[1]);
-//     var id = params.get('id');
-
-//     $http.get('https://api-fake-blog.onrender.com/postagens/' + id)
-//     .then(function(response) {
-//         $scope.noticia = response.data;
-//     }, function(error) {
-//         console.error('Erro ao buscar a notícia:', error);
-//     });
-// });
 
 fetch('https://api-fake-blog.onrender.com/postagens/').then(resposta=>{
     resposta.json().then((res=>{
